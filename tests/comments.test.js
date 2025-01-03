3533025ade7f2803a5592dab33027c1bfa4b960f
mayadahan77 <mayaschool555@gmail.com>
2025-01-03 15:53:15 +0200

    finish comments tests

--- tests/comments.test.js
+++ tests/comments.test.js
@@ -29,7 +29,7 @@ describe("Comments Tests", () => {
     expect(response.body.length).toBe(0);
   });
 
-  test("Test Create comment", async () => {
+  test("Test Create Comment", async () => {
     const responsePost = await request(app).post("/posts").send({
       title: "Test Post",
       content: "Test Content",
@@ -48,16 +48,15 @@ describe("Comments Tests", () => {
     expect(response.body.senderID).toBe("TestOwner");
   });
 
-  test("Test get comment by post ID", async () => {
-    const response = await request(app).get("/posts?post="+postId);
+  test("Test get Comments by post ID", async () => {
+    const response = await request(app).get("/comments/post/"+postId);
     expect(response.statusCode).toBe(200);
-    expect(response.body.length).toBe(1);
-    expect(response.body[0].title).toBe("Test Post");
-    expect(response.body[0].content).toBe("Test Content");
+    expect(response.body[0].postsID).toBe(postId)
+    expect(response.body[0].content).toBe("Test the comment");
     expect(response.body[0].senderID).toBe("TestOwner");
   });
 
-  test("Test get comment by id", async () => {
+  test("Test get Comment by id", async () => {
     const response = await request(app).get("/comments/" + commentId);
     expect(response.statusCode).toBe(200);
     expect(response.body.postsID).toBe(postId)
@@ -65,7 +64,30 @@ describe("Comments Tests", () => {
     expect(response.body.senderID).toBe("TestOwner");
   });
 
-  test("Test Create comment 2", async () => {
+  test("Test update Comment", async () =>{
+    const response2 = await request(app).put("/comments/" + commentId).send({
+      content: "wow this is amazing",
+    });
+    const response = await request(app).get("/comments/" + commentId);
+    expect(response.statusCode).toBe(200);
+    expect(response.body.content).toBe("wow this is amazing");
+  })
+
+  test("Test update non exists Comment", async () =>{
+    const response = await request(app).put("/comments/1234").send({
+      content: "wow this is amazing",
+    });
+    expect(response.statusCode).toBe(400);
+  })
+
+  test("Test update Comment with wrong parmeters", async () =>{
+    const response = await request(app).put("/comments" + commentId).send({
+      title: "wow this is amazing",
+    });
+    expect(response.statusCode).toBe(404);
+  })
+
+  test("Test Create Comment 2", async () => {
     const response = await request(app).post("/comments").send({
       postsID: postId,
       content: "Test the comment 2",
@@ -74,7 +96,7 @@ describe("Comments Tests", () => {
     expect(response.statusCode).toBe(201);
   });
 
-  test("Test get all 2 comment", async () => {
+  test("Test get all 2 Comment", async () => {
     const response = await request(app).get("/comments");
     expect(response.statusCode).toBe(200);
     expect(response.body.length).toBe(2);
@@ -86,5 +108,17 @@ describe("Comments Tests", () => {
       content: "Test Content 2",
     });
     expect(response.statusCode).toBe(400);
+  });
+
+  test("Test delete Comment", async () => {
+    const response = await request(app).delete("/comments/" + commentId);
+    expect(response.statusCode).toBe(200);
+    const getResponse = await request(app).get("/comments/" + commentId);
+    expect(getResponse.statusCode).toBe(404);
+  });
+
+  test("Test delete non exists Comment", async () => {
+    const response = await request(app).delete("/comments/1234");
+    expect(response.statusCode).toBe(500);
   });
 });
