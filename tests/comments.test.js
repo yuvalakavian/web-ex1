5a9b94726f9769b8ad29f0570e660d932c80eff3
mayadahan77 <mayaschool555@gmail.com>
2025-01-03 15:38:04 +0200

    add comments jest to project

--- tests/comments.test.js
+++ tests/comments.test.js
@@ -3,7 +3,8 @@ const request = require("supertest");
 const initApp = require("../server");
 const mongoose = require("mongoose");
 const postModel = require("../models/post");
-const commentModel = require("../models/comment")
+const commentModel = require("../models/comment");
+const { getCommentByPostId } = require("../controllers/comments_controller");
 
 var app;
 beforeAll(async () => {
@@ -20,6 +21,7 @@ afterAll((done) => {
 });
 
 let postId = "";
+let commentId = "";
 describe("Comments Tests", () => {
   test("Comments test get all", async () => {
     const response = await request(app).get("/comments");
@@ -27,27 +29,27 @@ describe("Comments Tests", () => {
     expect(response.body.length).toBe(0);
   });
 
-  test("Test Create Post", async () => {
+  test("Test Create comment", async () => {
     const responsePost = await request(app).post("/posts").send({
       title: "Test Post",
       content: "Test Content",
       senderID: "TestOwner"
     })
-    postId = responsePost.body._id;
+    postId = responsePost.body._id.toString().replace(/ObjectId\("(.*)"\)/, "$1");
     const response = await request(app).post("/comments").send({
       postsID: postId,
       content: "Test the comment",
       senderID: "TestOwner"
-    });
+    }); 
+    commentId = response.body._id
     expect(response.statusCode).toBe(201);
-    expect(response.body.title).toBe("Test Post");
-    expect(response.body.content).toBe("Test Content");
+    expect(response.body.postsID).toBe(postId)
+    expect(response.body.content).toBe("Test the comment");
     expect(response.body.senderID).toBe("TestOwner");
-    postId = response.body._id;
   });
 
-  test("Test get post by senderID", async () => {
-    const response = await request(app).get("/posts?senderID=TestOwner");
+  test("Test get comment by post ID", async () => {
+    const response = await request(app).get("/posts?post="+postId);
     expect(response.statusCode).toBe(200);
     expect(response.body.length).toBe(1);
     expect(response.body[0].title).toBe("Test Post");
@@ -55,31 +57,31 @@ describe("Comments Tests", () => {
     expect(response.body[0].senderID).toBe("TestOwner");
   });
 
-  test("Test get post by id", async () => {
-    const response = await request(app).get("/posts/" + postId);
+  test("Test get comment by id", async () => {
+    const response = await request(app).get("/comments/" + commentId);
     expect(response.statusCode).toBe(200);
-    expect(response.body.title).toBe("Test Post");
-    expect(response.body.content).toBe("Test Content");
+    expect(response.body.postsID).toBe(postId)
+    expect(response.body.content).toBe("Test the comment");
     expect(response.body.senderID).toBe("TestOwner");
   });
 
-  test("Test Create Post 2", async () => {
-    const response = await request(app).post("/posts").send({
-      title: "Test Post 2",
-      content: "Test Content 2",
-      senderID: "TestOwner2",
+  test("Test Create comment 2", async () => {
+    const response = await request(app).post("/comments").send({
+      postsID: postId,
+      content: "Test the comment 2",
+      senderID: "TestOwner2"
     });
     expect(response.statusCode).toBe(201);
   });
 
-  test("Posts test get all 2", async () => {
-    const response = await request(app).get("/posts");
+  test("Test get all 2 comment", async () => {
+    const response = await request(app).get("/comments");
     expect(response.statusCode).toBe(200);
     expect(response.body.length).toBe(2);
   });
 
-  test("Test Create Post fail", async () => {
-    const response = await request(app).post("/posts").send({
+  test("Test Create Comment fail", async () => {
+    const response = await request(app).post("/comments").send({
       title: "Test Post 2",
       content: "Test Content 2",
     });
