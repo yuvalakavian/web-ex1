31920fdd718f78fa0416c6f4e69b1695d9ce76bf
Yuval Akavian <yuvalmetal@gmail.com>
2025-01-03 14:51:20 +0200

    reverted to senderID

--- models/post.js
+++ models/post.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema({
-  userID: { type: String, required: true },
+  senderID: { type: String, required: true },
   content: { type: String, required: true },
   title: { type: String, required: true },
   createdAt: { type: Date, default: Date.now }
